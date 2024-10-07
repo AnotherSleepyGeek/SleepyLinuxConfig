d5ce52619fe9cf975a55674b25818e93f41ece1f
AnotherSleepyGeek <Francois.Vezina@outlook.com>
2024-10-07 12:07:06 -0400

    Initial version for ubuntu

---
+++ ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+        echo "This script must be run as root" 1>&2
+        exit 1
+fi
+
+
+apt update
+apt upgrade -y
+
+apt install -y iputils-ping dnsutils clamav clamav-daemon nano ufw
+
+Echo Updating Antivirus
+systemctl stop clamav-freshclam
+freshclam
+sudo systemctl start clamav-freshclam
+
+echo Enabling Firewall
+ufw allow ssh
+echo y | ufw enable
+
+
+
+#Copy my auhtorized key for authentication with ssh key
+cp -f  ./authorized_keys  ~/.ssh/authorized_keys .
+
+# Disable password authentication
+SSH_CONFIG="/etc/ssh/sshd_config"
+if grep -q "^#PasswordAuthentication yes" "$SSH_CONFIG"; then
+   sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/' "$SSH_CONFIG"
+else
+   echo "PasswordAuthentication no" >> "$SSH_CONFIG"
+fi
+
+
