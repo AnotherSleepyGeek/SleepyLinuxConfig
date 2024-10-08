2d72cca01d7331d515748fd528e1eb0b986d048c
AnotherSleepyGeek <Francois.Vezina@outlook.com>
2024-10-08 12:34:27 -0400

    Removed key from script.

--- ubuntu.sh
+++ ubuntu.sh
@@ -20,17 +20,3 @@ echo Enabling Firewall
 ufw allow ssh
 echo y | ufw enable
 
-
-
-#Copy my auhtorized key for authentication with ssh key
-cp -f  ./authorized_keys  ~/.ssh/authorized_keys .
-
-# Disable password authentication
-SSH_CONFIG="/etc/ssh/sshd_config"
-if grep -q "^#PasswordAuthentication yes" "$SSH_CONFIG"; then
-   sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/' "$SSH_CONFIG"
-else
-   echo "PasswordAuthentication no" >> "$SSH_CONFIG"
-fi
-
-
